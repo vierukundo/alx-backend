655a15c46d8938e40581a17501a88db89033fe81
vierukundo <vierukundo@gmail.com>
2024-01-27 22:39:44 +0000

    return dict of dataset

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        indices = index_range(page, page_size)
+        start_index = indices[0]
+        end_index = indices[1]
+
+        dataset = self.dataset()
+
+        # Return the appropriate page of the dataset
+        try:
+            return dataset[start_index:end_index]
+        except IndexError:
+            return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """returns a dictionary with information from dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        current_page = page
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        # Use get_page to get the dataset page
+        dataset_page = self.get_page(page, page_size)
+
+        # Calculate next_page and prev_page
+        next_page = current_page + 1 if current_page < total_pages else None
+        prev_page = current_page - 1 if current_page > 1 else None
+
+        data_dict = {
+            "page_size": len(dataset_page),
+            "page": current_page,
+            "data": dataset_page,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+        return data_dict
