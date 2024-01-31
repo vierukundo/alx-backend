37b5a88db4a5200dca242754e9f14018c83edc67
vierukundo <vierukundo@gmail.com>
2024-01-31 23:11:43 +0000

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Imports"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Must assign to the dictionary self.cache_data
+        the item value for the key key"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        return self.cache_data.get(key)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,2 @@
+# 0x01-caching
+This project enables to practice different Cache Replacement Policies such as MLU, LRU, etc
