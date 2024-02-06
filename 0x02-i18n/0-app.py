a318e1318362360594a114d312768c31844f3949
vierukundo <vierukundo@gmail.com>
2024-02-06 22:30:05 +0000

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Route that displays title and header"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
