748623c79c448ab0e57f868d6d50a1dd4d389af0
vierukundo <vierukundo@gmail.com>
2024-02-06 22:24:11 +0000

    Basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Flask app module"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Route that displays title and header"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
