7029afcb7c791b59b33f41218833b7adcb5fcb12
vierukundo <vierukundo@gmail.com>
2024-02-07 22:58:01 +0000

    Added translations

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Route that displays title and header"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
