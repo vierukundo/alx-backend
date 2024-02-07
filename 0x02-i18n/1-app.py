2690513093b67e9b2844dd9d4a8d2b284895dc7c
vierukundo <vierukundo@gmail.com>
2024-02-07 19:47:50 +0000

    configured babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Flask app module"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """Flask configurations class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Route that displays title and header"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
